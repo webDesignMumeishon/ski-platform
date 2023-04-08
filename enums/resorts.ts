b45e660871e2bcbb5bdb2f618d49d0b03220c991
Muma Dev <muma.sanmartin2011@gmail.com>
2023-04-08 14:26:21 -0300

    move enums

---
+++ enums/resorts.ts
@@ -0,0 +1,11 @@
+export enum States {
+    COLORADO = 'colorado'
+}
+
+export enum Towns {
+    BRECKENRIDGE = 'breckenridge',
+    BUTTERMILK = 'buttermilk',
+    ARAPAHOE_BASIN= 'arapahoe-basin',
+    ASPEN_HIGHLANDS= 'aspen-highlands',
+    ASPEN_MOUNTAIN= 'aspen-mountain',
+}
