edf36d83b119e5b93dd859abce87725301da1b88
TomasMacchi <muma.sanmartin2011@gmail.com>
2023-08-27 12:34:01 -0300

    add comments

--- .mocharc.js
+++ .mocharc.js
@@ -1,4 +1,11 @@
 
+/**
+ * mocharc.js (or simply mocha.opts for versions prior to Mocha 8.0.0) is a configuration file used to customize the behavior of the Mocha test runner. 
+ * This file allows you to define various options, settings, and command-line arguments that affect how your tests are executed. 
+ * By using a mocharc.js file, you can avoid passing command-line arguments every time you run Mocha
+ */
+
+
 const suite = process.env.TEST_SUITE || 'unit';
 const isUnit = suite === 'unit';
 const timeout = isUnit ? 50 : 3000;
