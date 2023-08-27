59e549810922c4e15678beaec2ea1727644e8cd7
TomasMacchi <muma.sanmartin2011@gmail.com>
2023-08-27 13:08:39 -0300

    add class for tests

--- .mocharc.js
+++ .mocharc.js
@@ -5,21 +5,41 @@
  * By using a mocharc.js file, you can avoid passing command-line arguments every time you run Mocha
  */
 
+const UNIT = 'unit'
 
-const suite = process.env.TEST_SUITE || 'unit';
-const isUnit = suite === 'unit';
-const timeout = isUnit ? 50 : 3000;
+class Test {
+	requiredFiles = [
+		'ts-node/register', // It allows you to directly run TypeScript code in Node.js without having to explicitly compile it to JavaScript first. By using ts-node/register, you're telling Node.js to use the TypeScript compiler (ts-node) to transpile your TypeScript code on-the-fly before executing it.
+		'test/config.ts', // Load config file to set up middlewares
+		'source-map-support/register', // When an error occurs, this utility helps translate the error location from the compiled JavaScript back to the original TypeScript source code.
+	];
 
-const requiredFiles = [
-	'ts-node/register', // It allows you to directly run TypeScript code in Node.js without having to explicitly compile it to JavaScript first. By using ts-node/register, you're telling Node.js to use the TypeScript compiler (ts-node) to transpile your TypeScript code on-the-fly before executing it.
-	'test/config.ts', // Load config file to set up middlewares
-	'source-map-support/register', // When an error occurs, this utility helps translate the error location from the compiled JavaScript back to the original TypeScript source code.
-];
+	constructor(suite){
+		this.suite = suite
+	}
+
+	getConfigs(){
+		const configs = {
+			require: this.requiredFiles
+		}
+
+		switch(this.suite){
+			case UNIT: 
+			default:
+				configs.timeout = 50
+				configs.spec = 'test/unit/**/*.spec.ts'
+		}
+
+		return configs
+	}
+}
+
+const suite = process.env.TEST_SUITE || UNIT;
+const suiteConfigs = new Test(suite)
 
 module.exports = {
-	timeout,
 	exit: true,
-	parallel: false,//isUnit,
+	parallel: false,
 	recursive: true,
-	require: requiredFiles
+	...suiteConfigs.getConfigs()
 };
